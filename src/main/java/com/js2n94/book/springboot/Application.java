69ad666fa681b284d541250eb9be65bfe81d3000
yoosi <yoosijisun12@gmail.com>
2021-04-07 14:32:44 +0900

    2장 스프링부트에서 테스트 코드를 작성하자

---
+++ src/main/java/com/js2n94/book/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.js2n94.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/js2n94/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.js2n94.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
